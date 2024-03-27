2500c393314e3508ae1af2916dea8f5c934cf455
Seiji Amashige <s-amashige@stf.kodansha.co.jp>
2024-03-27 13:07:32 +0900

    ビルド時間の計測

--- generate.js
+++ generate.js
@@ -3,6 +3,9 @@ import distribute from './lib/distribute.js'
 import { indexing } from './lib/indexer.js'
 import { srcDir, distDir } from './lib/dir.js'
 
+const start = performance.now()
 const data = await indexing(srcDir)
 
-distribute(data, srcDir, distDir)
+await distribute(data, srcDir, distDir)
+const end = performance.now()
+console.log('build: ' + (end - start) + "ms")
