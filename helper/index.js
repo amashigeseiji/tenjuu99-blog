13b749c1b4a84f7f757bd98f978b592dbdd18af5
amashigeseiji <tenjuu99@gmail.com>
2024-08-23 00:58:49 +0900

    部分テンプレート描画用のヘルパーを追加

--- helper/index.js
+++ helper/index.js
@@ -1,4 +1,5 @@
 import { readIndex as read } from "../lib/indexer.js";
+import { replaceVariablesFilter } from "../lib/filter.js";
 
 export async function readIndex () {
   return await read()
@@ -7,4 +8,8 @@ export async function readIndex () {
 export function dateFormat(dateString) {
   const date = new Date(dateString)
   return `${date.getFullYear()}年${date.getMonth() + 1}月${date.getDate()}日`
+}
+
+export function render(text, variables) {
+  return replaceVariablesFilter(text, variables)
 }
