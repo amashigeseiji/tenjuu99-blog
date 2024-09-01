6dcc903564827eb86595fac055f0d111e398a081
amashigeseiji <tenjuu99@gmail.com>
2024-09-02 03:52:52 +0900

    move 'generate' to be function

--- generate.js
+++ generate.js
@@ -1,11 +1,2 @@
-"use strict"
-import distribute from './lib/distribute.js'
-import { indexing } from './lib/indexer.js'
-import { srcDir, distDir } from './lib/dir.js'
-
-const start = performance.now()
-const data = await indexing(srcDir + '/pages/')
-
-await distribute(data, srcDir, distDir)
-const end = performance.now()
-console.log('build: ' + (end - start) + "ms")
+import generate from './lib/generate.js'
+generate()
