c6bf04ffa213400d4a8a0cca723d224abb77b931
amashigeseiji <tenjuu99@gmail.com>
2024-09-08 04:02:40 +0900

    editor が利用可能かどうか

--- src-sample/helper/index.js
+++ src-sample/helper/index.js
@@ -1,3 +1,9 @@
+import { allData } from '@tenjuu99/blog/lib/indexer.js'
+
 export function additionalHelper() {
   return 'これは追加ヘルパーによって出力されているメッセージです。'
+}
+
+export function isEditorEnabled() {
+  return allData.editor && allData.editor.distribute
 }
