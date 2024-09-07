5efffad9d5053ef11d25a38623642bc1968010a5
amashigeseiji <tenjuu99@gmail.com>
2024-09-07 22:40:30 +0900

    create editor

---
+++ src-sample/server/editor.js
@@ -0,0 +1,38 @@
+import { IncomingMessage, ServerResponse } from 'http'
+import fs from 'node:fs/promises'
+import { styleText } from 'node:util'
+
+const config = (await import('../../lib/config.js')).default
+const dir = (await import('../../lib/dir.js'))
+
+export const path = '/editor'
+
+/**
+ * @param {IncomingMessage} req
+ * @param {ServerResponse} res
+ */
+export const post = async (req, res) => {
+  const chunks = []
+  req
+    .on('data', (chunk) => chunks.push(chunk))
+    .on('end', async () => {
+      const json = JSON.parse(chunks.join())
+      const file = json.inputFileName ? json.inputFileName : json.selectDataFile
+      if (!file) {
+        res.writeHead(401, { 'content-type': 'application/json' })
+        res.end(JSON.stringify({
+          'message': 'ファイル名がありません'
+        }))
+        return
+      }
+      await fs.writeFile(`${dir.pageDir}/${file}`, json.content)
+      console.log(styleText('blue', '[editor/post] finished'))
+
+      const href = file.split('.')[0]
+      res.writeHead(200, { 'content-type': 'application/json' })
+      res.end(JSON.stringify({
+        'href': `/${href}`
+      }))
+    })
+  return true
+}
