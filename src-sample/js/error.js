236c00d64e22e6671929a6cd525daa1d5e30d5a6
amashigeseiji <tenjuu99@gmail.com>
2024-09-10 00:30:28 +0900

    fix editor style

--- src-sample/js/error.js
+++ src-sample/js/error.js
@@ -4,4 +4,5 @@ const url = new URL(location)
 if (url.pathname !== '/editor') {
   const link = document.querySelector('.editor_link')
   link.href = `/editor?md=${url.pathname.replace('/', '')}.md`
+  link.innerHTML = `${url.pathname.replace('/', '')}のページを作成する`
 }
