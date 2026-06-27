ba52e588016a15017634033985e1fd49df39b8be
Seiji Amashige <tenjuu99@gmail.com>
2026-06-28 06:24:35 +0900

    プレビューでcssリクエストをしないで済ませる
    npm run dev で起動しているとき、自動保存で再起動がかかるので、再起動中に
    cssリクエストがかかるとエラーになりスタイルが崩れる
    インライン化しておけばこの事象にはならない

--- packages/editor/server/preview.js
+++ packages/editor/server/preview.js
@@ -2,8 +2,38 @@ import { IncomingMessage, ServerResponse } from 'http'
 import { styleText } from 'node:util'
 import render from '@tenjuu99/blog/lib/render.js'
 import makePageData from '@tenjuu99/blog/lib/pageData.js'
+import { distDir, srcDir } from '@tenjuu99/blog/lib/dir.js'
+import fs from 'node:fs'
 
 export const path = '/preview'
+
+/**
+ * <link rel="stylesheet"> タグを読み込んでインライン <style> に置換する。
+ * プレビューiframeがサーバー再起動中のCSSリクエスト失敗を起こさないようにするため。
+ * @param {string} html
+ * @returns {string}
+ */
+function inlineStyles(html) {
+  return html.replace(
+    /<link\b[^>]*\brel=["']stylesheet["'][^>]*\/?>/gi,
+    (match) => {
+      const hrefMatch = match.match(/\bhref=["']([^"']+)["']/)
+      if (!hrefMatch) return match
+      const href = hrefMatch[1]
+      if (href.startsWith('http://') || href.startsWith('https://') || href.startsWith('//')) {
+        return match
+      }
+      const filePath = href.split('?')[0]
+      for (const dir of [distDir, srcDir]) {
+        const fullPath = `${dir}${filePath}`
+        if (fs.existsSync(fullPath)) {
+          return `<style>${fs.readFileSync(fullPath, 'utf8')}</style>`
+        }
+      }
+      return match
+    }
+  )
+}
 
 /**
  * @param {IncomingMessage} req
@@ -26,7 +56,7 @@ export const post = async (req, res) => {
       const rendered = await render(pageData.template, pageData)
       res.writeHead(200, { 'content-type': 'application/json' })
       res.end(JSON.stringify({
-        'preview': rendered
+        'preview': inlineStyles(rendered)
       }))
     })
   return true
